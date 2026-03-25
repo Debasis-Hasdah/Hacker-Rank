03256a8b73af38e4d66974762e44d561e2441ff3
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-26 00:13:25 +0530

    solved grading problem

---
+++ GradingStudentsExample.java
@@ -0,0 +1,35 @@
+import java.util.*;
+
+public class GradingStudentsExample {
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+
+        List<Integer> result = new ArrayList<>();
+
+        for (int grade : grades) {
+
+            if (grade >= 38) {
+
+                int nextMultiple = ((grade / 5) + 1) * 5;
+
+                if (nextMultiple - grade < 3) {
+                    grade = nextMultiple;
+                }
+            }
+
+            result.add(grade);
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> grades = Arrays.asList(73, 67, 38, 33);
+
+        List<Integer> result = gradingStudents(grades);
+
+        System.out.println("Original: " + grades);
+        System.out.println("Rounded : " + result);
+    }
+}
