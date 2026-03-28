f15c2f374d5860f943d5f65b7ec0fc7dfb717791
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-29 00:58:38 +0530

    solved breaking record problem

---
+++ BreakingRecordsExample.java
@@ -0,0 +1,38 @@
+import java.util.*;
+
+public class BreakingRecordsExample {
+
+    public static List<Integer> breakingRecords(List<Integer> scores) {
+
+        int maxScore = scores.get(0);
+        int minScore = scores.get(0);
+
+        int maxCount = 0;
+        int minCount = 0;
+
+        for (int i = 1; i < scores.size(); i++) {
+
+            int score = scores.get(i);
+
+            if (score > maxScore) {
+                maxScore = score;
+                maxCount++;
+            } 
+            else if (score < minScore) {
+                minScore = score;
+                minCount++;
+            }
+        }
+
+        return Arrays.asList(maxCount, minCount);
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> scores = Arrays.asList(10, 5, 20, 20, 4, 5, 2, 25, 1);
+
+        List<Integer> result = breakingRecords(scores);
+
+        System.out.println("Result: " + result);
+    }
+}
