aef9a4e7991a46a68d01a7667f5631470834eb4c
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-30 23:47:03 +0530

    solved migratory birds problem

---
+++ MigratoryBirdsExample.java
@@ -0,0 +1,36 @@
+import java.util.*;
+
+public class MigratoryBirdsExample {
+
+    public static int migratoryBirds(List<Integer> arr) {
+
+        int[] count = new int[6]; // index 1 to 5
+
+        // count frequency
+        for (int bird : arr) {
+            count[bird]++;
+        }
+
+        int max = 0;
+        int result = 0;
+
+        // find most frequent (smallest id if tie)
+        for (int i = 1; i <= 5; i++) {
+            if (count[i] > max) {
+                max = count[i];
+                result = i;
+            }
+        }
+
+        return result;
+    }
+
+    public static void main(String[] args) {
+
+        List<Integer> arr = Arrays.asList(1, 4, 4, 4, 5, 3);
+
+        int ans = migratoryBirds(arr);
+
+        System.out.println("Most common bird type: " + ans);
+    }
+}
