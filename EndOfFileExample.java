072b873a92ed4e32590b999985fabc6f2f704b76
Debasis-Hasdah <debasishasdah@gmail.com>
2026-03-31 21:33:25 +0530

    solved end of file

---
+++ EndOfFileExample.java
@@ -0,0 +1,18 @@
+public class EndOfFileExample {
+
+    public static void main(String[] args) {
+
+        String[] lines = {
+            "Hello world",
+            "I am a file",
+            "Read me until end-of-file."
+        };
+
+        int i = 1;
+
+        for (String line : lines) {
+            System.out.println(i + " " + line);
+            i++;
+        }
+    }
+}
