e753f33bf5db5d8e62ea8c3ea563d77d87aaff5c
Debasis-Hasdah <debasishasdah@gmail.com>
2026-04-02 01:20:13 +0530

    solved time convention from hr

---
+++ TimeConversionVSCode.java
@@ -0,0 +1,36 @@
+public class TimeConversionVSCode {
+
+    public static String timeConversion(String s) {
+
+        String period = s.substring(8);   // AM or PM
+        int hour = Integer.parseInt(s.substring(0, 2));
+        String rest = s.substring(2, 8); // :mm:ss
+
+        if (period.equals("AM")) {
+            if (hour == 12) {
+                hour = 0;
+            }
+        } else { // PM
+            if (hour != 12) {
+                hour += 12;
+            }
+        }
+
+        return String.format("%02d", hour) + rest;
+    }
+
+    public static void main(String[] args) {
+
+        String[] tests = {
+            "07:05:45PM",
+            "12:01:00AM",
+            "12:01:00PM",
+            "01:00:00AM",
+            "11:59:59PM"
+        };
+
+        for (String t : tests) {
+            System.out.println(t + " → " + timeConversion(t));
+        }
+    }
+}
