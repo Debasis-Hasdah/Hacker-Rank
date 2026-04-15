8319c339d1db943b191664854cf30ba2ae3c5d14
Debasis-Hasdah <debasishasdah@gmail.com>
2026-04-15 23:31:02 +0530

    solved drawing  book problem

---
+++ DrawingBookExample.java
@@ -0,0 +1,19 @@
+public class DrawingBookExample {
+
+    public static int pageCount(int n, int p) {
+
+        int fromFront = p / 2;
+        int fromBack = (n / 2) - (p / 2);
+
+        return Math.min(fromFront, fromBack);
+    }
+
+    public static void main(String[] args) {
+
+        int n1 = 6, p1 = 2;
+        int n2 = 5, p2 = 4;
+
+        System.out.println("Example 1: " + pageCount(n1, p1));
+        System.out.println("Example 2: " + pageCount(n2, p2));
+    }
+}
