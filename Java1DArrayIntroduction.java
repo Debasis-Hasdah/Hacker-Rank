4eb83fb73fc5b0c79b6f7deb9f5d758318bb38b1
Debasis-Hasdah <debasishasdah@gmail.com>
2026-05-11 21:16:04 +0530

    solved 1d array

---
+++ Java1DArrayIntroduction.java
@@ -0,0 +1,13 @@
+public class Java1DArrayIntroduction {
+
+    public static void main(String[] args) {
+
+        // Create array
+        int[] arr = {10, 20, 30, 40, 50};
+
+        // Print array elements
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println(arr[i]);
+        }
+    }
+}
