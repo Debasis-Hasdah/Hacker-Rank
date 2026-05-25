ab953fd8a7036c36ebe052dbfbb2fe60f4df7484
Debasis-Hasdah <debasishasdah@gmail.com>
2026-05-25 21:36:10 +0530

    solved a string problem

---
+++ JavaStringCompare.java
@@ -0,0 +1,31 @@
+public class JavaStringCompare {
+
+    public static void main(String[] args) {
+
+        String s = "welcometojava";
+        int k = 3;
+
+        String smallest = s.substring(0, k);
+        String largest = s.substring(0, k);
+
+        // Generate all substrings of length k
+        for (int i = 0; i <= s.length() - k; i++) {
+
+            String current = s.substring(i, i + k);
+
+            // Compare for smallest
+            if (current.compareTo(smallest) < 0) {
+                smallest = current;
+            }
+
+            // Compare for largest
+            if (current.compareTo(largest) > 0) {
+                largest = current;
+            }
+        }
+
+        // Output
+        System.out.println("Smallest: " + smallest);
+        System.out.println("Largest: " + largest);
+    }
+}
