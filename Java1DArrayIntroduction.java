5ecaa8d6817d9fcabcdcbbac5b1e3386767f57e9
Debasis-Hasdah <debasishasdah@gmail.com>
2026-05-26 22:24:43 +0530

    make some changes in the code

--- Java1DArrayIntroduction.java
+++ Java1DArrayIntroduction.java
@@ -2,10 +2,11 @@ public class Java1DArrayIntroduction {
 
     public static void main(String[] args) {
 
-        // Create array
+       
         int[] arr = {10, 20, 30, 40, 50};
 
         // Print array elements
+        System.out.println("here the array element");
         for (int i = 0; i < arr.length; i++) {
             System.out.println(arr[i]);
         }
